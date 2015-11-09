3d54e2cada2ad909552dcb3fff07e81be84673f1
hdurenl430 <hlfgoo@gmail.com>
2015-11-09 14:44:19 +0800

    first commit

---
+++ src/main/java/com/simon/myssh/HelloAction.java
@@ -0,0 +1,18 @@
+package com.simon.myssh;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloAction extends ActionSupport{
+
+	/* (non-Javadoc)
+	 * @see com.opensymphony.xwork2.ActionSupport#execute()
+	 */
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return super.execute();
+	}
+	
+	
+
+}
---
+++ target/MySsh/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>hello! spring struts 1</h2>
+<form action="hello">
+<input type="submit">
+</form>
+</body>
+</html>
